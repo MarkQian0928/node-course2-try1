e72e2ea7388ba95cfbff7a7ba04994848b56cc80
TIANJIAO QIAN <markqian199413@gmail.com>
2017-07-04 21:20:36 -0600

    clean the app.js file

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 console.log('starting app');
-
+//const os =require('os');
 const fs = require('fs');
-const os =require('os');
 const _ = require('lodash');
-//const notes = require('./node.js');
+
+const notes = require('./node.js');
 
 console.log(_.isString(true));
 console.log(_.isString("Mark"));
 var filteredArray = _.uniq(['mark',1,'Mark','mark', 3,11,3,4]);
 console.log(filteredArray);
+
+
+
+
 
 /*var user = os.userInfo();
 console.log(user);
