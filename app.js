3041c37108978d66e0bc24d792d4fea3383ec7b2
TIANJIAO QIAN <markqian199413@gmail.com>
2017-07-04 21:48:52 -0600

    test for course 13 done, get input from command line

--- app.js
+++ app.js
@@ -1,24 +1,7 @@
 console.log('starting app');
-//const os =require('os');
 const fs = require('fs');
 const _ = require('lodash');
 
 const notes = require('./node.js');
 
-console.log(_.isString(true));
-console.log(_.isString("Mark"));
-var filteredArray = _.uniq(['mark',1,'Mark','mark', 3,11,3,4]);
-console.log(filteredArray);
-
-
-
-
-
-/*var user = os.userInfo();
-console.log(user);
-
-fs.appendFile('greeting.txt', `\nHello, ${user.username}`, function(err){
-    if (err){
-        console.log('unable to write to file');
-    }
-});*/
+console.log(process.argv);
