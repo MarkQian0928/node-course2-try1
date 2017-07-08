e524a1e6bbe60e14274d459688577db71e921b0c
TIANJIAO QIAN <markqian199413@gmail.com>
2017-07-08 00:47:30 -0600

    course 16 add and save notes finished

--- node.js
+++ node.js
@@ -1,7 +1,26 @@
 console.log('this is node.js');
 
+const fs = require('fs');
+
 var addNote = (title, body)=>{
-    console.log("Adding note", title, body);
+    var notes =[];
+    var note = {
+        title,
+        body
+    };
+
+    try{
+        var notesString = fs.readFileSync('note-date.json');
+        notes = JSON.parse(notesString);
+    }catch(e){
+
+    }
+
+    var duplicateNote = notes.filter((note)=>title === note.title);
+    if(duplicateNote.length ===0){
+        notes.push(note);
+        fs.writeFileSync('note-date.json', JSON.stringify(notes));
+    }
 };
 
 var getAll =()=>{
